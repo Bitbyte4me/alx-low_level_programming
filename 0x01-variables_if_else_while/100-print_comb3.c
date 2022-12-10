b297c60cfdfd64a212b82a7147d07714d811a914
Olawale <horlah2me@gmail.com>
2022-12-09 21:16:33 -0800

    brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - Prints all possible combinations of two different digits
  * Return: Always 0
  */
 int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 == 9 && digit2 == 9)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 		}
 	}
