bb9e4fb9cc86a830c8649ef4ac68e04105fec438
Olawale <horlah2me@gmail.com>
2022-12-13 15:36:10 -0800

    multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * times_table - Prints a multiplication table up to param
+ * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
+ *
  * Return: Number matrix
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 	int x, y, z;
 
@@ -21,6 +22,14 @@ void times_table(int n)
 					_putchar(',');
 					_putchar(32);
 					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
