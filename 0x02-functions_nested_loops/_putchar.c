5d6908c82ad1d589c011d89c584ed0ff3792a58a
Olawale <horlah2me@gmail.com>
2022-12-13 06:10:22 -0800

    putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 
 /**
- * main - write the character c to stdout gc: the character to print
- * Return: on success
+ * main - write the character c to stdout
+ * gc: the character to print
+ *
+ * Return: on success 1
  */
 int _putchar(char c)
 {
