867c051f5d389cfb302cf210243f55386be1b5cd
Olawale <horlah2me@gmail.com>
2022-12-13 05:48:30 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar followed by a new line
  * Return: Always 0
  */
 int main(void)
 {
-	char *sh = "Main";
-
-	while (*sh)
-	{
-		_putchar(*sh)
-			sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - write the character c to stdout gc: the character to print
+ * Return: on success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
