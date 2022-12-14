dc70258b82505995f4cee94ae473eb821ad96f89
Olawale <horlah2me@gmail.com>
2022-12-13 17:40:19 -0800

    mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,42 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints a multiplication table up to param
+ * print_times_table - prints the n times table, starting with 0
  * @n: The number to be treated
- * Return: Always 0
+ * Return: 
  */
-void print_times_table(int n);
+void print_times_table(int n)
 {
-	int x, y, z;
+	int num, mult, prod;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (num = 0; num <= n; num++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				z = x * y;
-				if (z > 99)
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (z > 9)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
