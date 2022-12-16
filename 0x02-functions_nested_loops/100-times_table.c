6ae7fd3728da399741aa66da7499dabc06932570
Olawale <horlah2me@gmail.com>
2022-12-15 16:42:54 -0800

    mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,51 @@
 #include "main.h"
+#include <stdio>
 
 /**
- * print_times_table - Entry point
+ * print_times_table - prints the n times table, starting with 0
  * @n: input
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
 {
+	int row;
+	int column;
+	int product;
 
-	return (0);
+	if (n < || n >= 15)
+		return;
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
+		}
+
+		_putchar('\n');
+	}
 }
