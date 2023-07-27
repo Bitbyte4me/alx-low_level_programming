79b082f5d9e0fc7fcdf7af97eaf5185569588bbc
Olawale <bitbyte4me@gmail.com>
2023-07-27 13:52:59 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,11 +2,10 @@
 #include <string.h>
 
 /**
-  * add_node_end - A fxn that adds a node to the end of linked list.
-  * @head: pointer to a list_t pointer that points to the head struct.
-  * @str: string to add as node.
+  * add_node_end - A fxn that adds a node to the end of linked list
+  * @head: pointer to a list_t pointer that points to the head struct
+  * @str: string to add as node
   *
-  * 
   * Return: pointer to new element of list, NULL on failure.
   */
 list_t *add_node_end(list_t **head, const char *str)
